a77fdcd78443eaf3b6ce0a52fc48706f9547a64d
do07tk <pisum.do.0713@gmail.com>
2023-03-27 12:37:23 +0900

    デプロイdb設定

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,10 @@ ActiveRecord::Schema.define(version: 2023_03_24_080610) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.string "profile"
     t.string "occupation"
     t.string "position"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
