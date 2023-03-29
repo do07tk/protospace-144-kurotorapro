4fe051b1e448c3f9e6b4ff02bc94731041ce9162
do07tk <pisum.do.0713@gmail.com>
2023-03-29 16:29:02 +0900

    修正

--- app/controllers/protos_controller.rb
+++ app/controllers/protos_controller.rb
@@ -1,4 +1,6 @@
 class ProtosController < ApplicationController
+  before_action :authenticate_user!, only: [:new] 
+
   def index
     @protos = Proto.includes(:user)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @protos = user.protos
+    @username = user.name
+    @userprofile = user.profile
+    @useroccupation = user.occupation
+    @userposition = user.position
   end  
 end
